d5d15c7e74076ccaf97fddbc22d7e41399755849
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-08-23 23:48:37 -0400

    remove gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === "PROD" ? "/python-dsa/" : "/",
-    plugins: [react()],
-  };
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
 });
