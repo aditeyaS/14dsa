2905b437856e61518c910d087bdf07f13992f701
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-08-23 23:45:34 -0400

    gh-pages-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === "production" ? "/python-dsa/" : "/",
+    base: mode === "PROD" ? "/python-dsa/" : "/",
     plugins: [react()],
   };
 });
