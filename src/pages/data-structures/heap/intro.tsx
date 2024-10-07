c717291d5ce3ac09b99aaf0445edb6cd5ac316c5
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-10-07 17:29:09 -0400

    heap1

--- src/pages/data-structures/heap/intro.tsx
+++ src/pages/data-structures/heap/intro.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Alert,
   CodeBlock,
@@ -7,86 +8,39 @@ import {
   Section,
   UL,
 } from "../../../components";
-
-const implementation = `class MinHeap:
-  def __init__(self):
-    self.mh = []
-  
-  def __init__(self, l):
-    self.mh = l
-    i = (len(l) - 2) // 2
-    while i >= 0:
-      self.minHeapify(i)
-      i -= 1
-    
-  def parent(self, i):
-    return (i - 1)//2
-  
-  def left(self, i):
-    return (2 * i + 1)
-  
-  def right(self, i):
-    return (2 * i + 2)
-  
-  def insert(self, x):
-    self.mh.append(x)
-    i = len(self.mh) - 1
-    while i > 0 and self.mh[self.parent(i)] > self.mh[i]:
-      p = self.parent(i)
-      self.mh[i], self.mh[p] = self.mh[p], self.mh[i]
-      i = p
-
-  def minHeapify(self, i):
-    l = self.left(i)
-    r = self.right(i)
-    smallest = i
-    n = len(self.mh)
-    if l < n and self.mh[l] < self.mh[smallest]:
-      smallest = l
-    if r < n and self.mh[r] < self.mh[smallest]:
-      smallest = r
-    if smallest != i:
-      self.mh[smallest], self.mh[i] = self.mh[i], self.mh[smallest]
-      self.minHeapify(smallest)
-      
-  def extractMin(self):
-    n = len(self.mh)
-    if n == 0:
-      return math.inf
-    res = self.mh[0]
-    self.mh[0] = self.mh[n - 1]
-    self.mh.pop()
-    self.minHeapify(0)
-    return res
-    
-  def decreaseKey(self, i, x):
-    self.mh[i] = x
-    while i != 0 and self.mh[self.parent(i)] > self.mh[i]:
-      p = self.parent(i)
-      self.mh[i], self.mh[p] = self.mh[p], self.mh[i]
-      i = p
-  
-  def deleteKey(self, i):
-    n = len(self.mh)
-    if i >= n:
-      return
-    self.decreaseKey(i, -math.inf)
-    self.extractMin()`;
-
-const heapq = `import heapq
-pq = [5,20,1,30,4]
-
-heapq.heapify(pq) #1,4,5,30,20
-heapq.heappush(pq,3) #1,4,3,30,20,5
-print(heapq.heappop(pq)) #3,4,5,30,20 (removes min element)
-
-heapq.nlargest(2,pq) #[30,20]
-heapq.nsmallest(2,pq) #[1,4]
-
-heapq.pushpop(pq,2)
-heapq.heapreplace(pq,-1)`;
+import ApiClient from "../../../lib/api-client";
 
 export const Intro = () => {
+  const apiClient = new ApiClient();
+  const [minHeap, setMinHeap] = useState<string>("");
+  const [heapq, setHeapq] = useState<string>("");
+
+  useEffect(() => {
+    const fetchMinHeap = async () => {
+      try {
+        const data = await apiClient.getCode(
+          "data-structures/heap/min_heap.py"
+        );
+        setMinHeap(data);
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      }
+    };
+    fetchMinHeap();
+  }, []);
+
+  useEffect(() => {
+    const fetchHeapq = async () => {
+      try {
+        const data = await apiClient.getCode("data-structures/heap/heapq.py");
+        setHeapq(data);
+      } catch (error) {
+        console.error("Failed to fetch data", error);
+      }
+    };
+    fetchHeapq();
+  }, []);
+
   return (
     <PageSectionContainer>
       <Section>
@@ -110,7 +64,7 @@ export const Intro = () => {
             = (i-1)//2
           </LI>
         </UL>
-        <CodeBlock>{implementation}</CodeBlock>
+        <CodeBlock>{minHeap}</CodeBlock>
       </Section>
       <Section>
         <H2>heapq in Python</H2>
